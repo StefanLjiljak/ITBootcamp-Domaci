e680f73669123d107e2053a6cce6c0b0a4df4d87
StefanLjiljak <stefanljiljak@gmail.com>
2019-11-05 23:56:09 +0100

    edit z1

--- Nedelja3/zadatak1.js
+++ Nedelja3/zadatak1.js
@@ -1,4 +1,7 @@
-function MaxNum(a, b, c) {
-    return Math.max(a, b, c)
-};
-console.log(MaxNum(1, 2, 3));
+function numMax(a, b, c) {
+    if (a > b && a > c) return a;
+    else if (b > a && b > c) return b;
+    else if (c > a && c > b) return c;
+    else return `Two or more equal num`;
+}
+console.log(numMax(5, 6, 6));
