92184f64dfcbf4493d38c15f9eddb59514f09f10
StefanLjiljak <stefanljiljak@gmail.com>
2019-12-01 22:10:43 +0100

    ispravka domaceg

--- Nedelja6/index.js
+++ Nedelja6/index.js
@@ -1,30 +1,30 @@
-function Validate() {
-    if (!((user.value.length >= 3) && (user.value.length <= 15) && (pass.value.length >= 3) && (pass.value.length <= 15))) {
-        alert('Neodgovarajuca duzina')
-    }
-    if (!isNaN(user.value.charAt(0))) {
-        alert('Prvi karakter User-a ne sme biti broj')
-    }
-
-    console.log(Number(user.value.charAt(0)))
-}
-
-btn.onclick = Validate;
-
-// Validate radi dok ne ukljucim JsonChek ispod, pretpostavljam zato sto je JSON na lokalu
+let dataUse;
+fetch('index.json')
+    .then(response => response.json())
+    .then(data => dataUse = data);
 
 btn.onclick = JsonCheck;
 
-function JsonCheck(e) {
-    fetch('./index.json').then(response => response.json())
-        .then(data => data = data.filter(x => x.username === user.value && x.password === pass.value));
-    if (data) {
-        alert('Ulogovan')
+
+function JsonCheck() {
+    let check = dataUse.some(x =>
+        x.username == user.value && x.password == pass.value
+    );
+
+    if (check) {
+        alert('logged')
+    } else if (!((user.value.length >= 3) && (user.value.length <= 15) && (pass.value.length >= 3) && (pass.value.length <= 15))) {
+        alert('Neodgovarajuca duzina')
+    } else if (!isNaN(user.value.charAt(0))) {
+        alert('Prvi karakter User-a ne sme biti broj')
     } else {
-        'Pogresan Username ili Pass'
+        alert('incorrect')
     }
+
 };
 
+
 /* 5.  fetch(server) plus metod POST input.values u requestu nakon cega server
- vrsi obradu i proveru u svojoj bazi i vraca nazad response true/false nakon cega mi radimo
- sa tim responsom dalju obradu tj sta vec zelimo.*/
+     vrsi obradu i proveru u svojoj bazi i vraca nazad response true/false nakon cega mi radimo
+     sa tim responsom dalju obradu tj sta vec zelimo.
+*/
